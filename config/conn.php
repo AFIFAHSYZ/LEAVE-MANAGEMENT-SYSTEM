5c6b26386abca754e8ead6762735196242cc1d3e
AFIFAH SYAZA HUDA BINTI AHMAD <117212342+AFIFAHSYZ@users.noreply.github.com>
2025-12-23 17:01:25 +0800

    Update database connection settings and error handling
    Refactored config/conn.php to use explicit DSN parameters, updated credentials, enabled error reporting, and improved error messages for database connection failures. Also removed redundant lines and cleaned up the code. Duplicate entry for config/conn.php was added to .gitignore.

--- config/conn.php
+++ config/conn.php
@@ -1,18 +1,15 @@
 <?php
-$host = 'localhost';
-$dbname = 'teraju';
-$user = 'postgres';
-$password = '#####';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$dsn = "pgsql:host=127.0.0.1;port=5432;dbname=teraju";
+$user = "postgres";
+$password = "test123";
+
 try {
-    $dsn = "pgsql:host=$host;dbname=$dbname";
     $pdo = new PDO($dsn, $user, $password);
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //echo "Database connected successfully!";
+    //echo "✅ Database connected successfully!";
 } catch (PDOException $e) {
-   die("Database connection failed: " . $e->getMessage());
+    echo "❌ Database connection failed:<br>";
+    echo $e->getMessage();
 }
-
-
-?>
